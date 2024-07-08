882153aa371440ebf07c95a9afa6a6f0ea7fdda1
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2024-07-08 22:44:34 +0300

    OD: string benchmarking

--- H.Benchmarks.CLI/BLL/Benchmarking/Benchmarks/StringBenchmarks.cs
+++ H.Benchmarks.CLI/BLL/Benchmarking/Benchmarks/StringBenchmarks.cs
@@ -1,5 +1,8 @@
 ﻿using BenchmarkDotNet.Attributes;
 using H.Necessaire;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace H.Benchmarks.CLI.BLL.Benchmarking.Benchmarks;
 
@@ -7,13 +10,58 @@ namespace H.Benchmarks.CLI.BLL.Benchmarking.Benchmarks;
 [Config(typeof(DefaultBenchmarksConfig))]
 public class StringBenchmarks : ImABenchmarkContainer
 {
+    const int numberOfStringsToConcatenate = 100;
+    static readonly string[] strings
+        = Enumerable
+        .Range(0, numberOfStringsToConcatenate)
+        .Select(i => i.ToString())
+        .ToArray()
+        ;
     public string Name { get; } = "String Benchmarks";
-
     public string Description { get; } = "String operations benchmarks.";
 
     [Benchmark]
     public string SimpleStringAllocation()
     {
         return "Test, Test, Testicles";
+    }
+
+    [Benchmark]
+    public string StringConcatenation_Via_Add_Operator()
+    {
+        string result = "";
+        foreach (string s in strings)
+        {
+            result += s;
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public string StringConcatenation_Via_Interpolation()
+    {
+        string result = "";
+        foreach (string s in strings)
+        {
+            result = $"{result}{s}";
+        }
+        return result;
+    }
+
+    [Benchmark]
+    public string StringConcatenation_Via_StringBuilder()
+    {
+        StringBuilder resultBuilder = new StringBuilder();
+        foreach (string s in strings)
+        {
+            resultBuilder.Append(s);
+        }
+        return resultBuilder.ToString();
+    }
+
+    [Benchmark]
+    public string StringConcatenation_Via_String_Join()
+    {
+        return string.Join("", strings);
     }
 }
--- H.Benchmarks.CLI/BLL/Benchmarking/DefaultBenchmarksConfig.cs
+++ H.Benchmarks.CLI/BLL/Benchmarking/DefaultBenchmarksConfig.cs
@@ -1,6 +1,5 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
-using BenchmarkDotNet.Diagnostics.Windows;
 
 namespace H.Benchmarks.CLI.BLL.Benchmarking;
 
